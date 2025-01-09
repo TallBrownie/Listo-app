79975f4abc87f4626692d0411fdc17314ed1c57b
TallBrownie <126225168+TallBrownie@users.noreply.github.com>
2025-01-09 21:03:26 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
+
+LISTS = [
+  {
+    'id': 1,
+    'title': 'To-Do',
+    "type": "Task List"
+  },
+
+  {
+    'id': 2,
+    'title': 'Grocery',
+    "type": "Grocery items"
+  },
+
+  {
+    'id': 3,
+    'title': 'Instruction',
+    "type": "List of Instructions"
+  },
+
+  {
+    'id': 4,
+    'title': 'Custom',
+    "type": "Customized List"
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', lists=LISTS)
+
+@app.route("/api/lists")
+def task_list():
+  return jsonify(LISTS)
 
 print (__name__)
 if __name__ == '__main__':
